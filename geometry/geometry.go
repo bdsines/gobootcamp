996a8ceb59fa6568b1ef8642feafc6d773fd23b5
bdsines <bdsines007@gmail.com>
2018-04-02 06:15:55 -0500

    Changing it to global go exercise repo

---
+++ geometry/geometry.go
@@ -0,0 +1,20 @@
+package main
+import (
+"fmt"
+"log"
+"geometry/rectangle"
+)
+var rectLen, rectWidth float64 = 6, 7;
+func init() {  
+	fmt.Printf("Initializing geometry \n")
+	if rectLen<0 {
+		log.Fatal("Length cannot be negative");
+	}
+	if rectWidth<0 {
+		log.Fatal("Width cannot be negative");
+	}
+}
+func main(){
+
+fmt.Printf("area of rectangle %.2f\n", rectangle.Area(rectLen, rectWidth));
+}
