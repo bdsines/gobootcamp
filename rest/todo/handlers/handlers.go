eb890e1b15fc321350fbf89183439afeae6b65e0
bdsines <bdsines007@gmail.com>
2018-04-08 11:22:24 -0500

    mongo changes

--- rest/todo/handlers/handlers.go
+++ rest/todo/handlers/handlers.go
@@ -14,42 +14,34 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// var todos repos.Todos
-
-// func Start() {
-
-// 	router := mux.NewRouter().StrictSlash(true)
-// 	router.HandleFunc("/", Index)
-// 	router.HandleFunc("/todos", TodoIndex)
-// 	router.HandleFunc("/todos/{todoId}", ShowTodo)
-// 	log.Fatal(http.ListenAndServe(":8080", router))
-// }
-
-func Index(w http.ResponseWriter, r *http.Request) {
+func Hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
 func TodoCreate(w http.ResponseWriter, r *http.Request) {
 	var todo repos.Todo
+	defer r.Body.Close()
 	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
 	if err != nil {
 		panic(err)
 	}
-	if err := r.Body.Close(); err != nil {
-		panic(err)
-	}
 	if err := json.Unmarshal(body, &todo); err != nil {
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		w.WriteHeader(422) // unprocessable entity
-		if err := json.NewEncoder(w).Encode(err); err != nil {
-			panic(err)
-		}
+		respondWithError(w, http.StatusBadRequest, " Bad Payload")
+		return
 	}
+
+	// if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
+	// 	panic(err)
+	// 	respondWithError(w, http.StatusBadRequest, "Bad Request")
+	// 	return
+	// }
+	// log.Printf("todo unmarshalled ", todo)
 	t := repos.CreateTodo(todo)
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(t); err != nil {
-		panic(err)
-	}
+	respondWithJson(w, http.StatusOK, t)
+	// w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	// w.WriteHeader(http.StatusOK)
+	// if err := json.NewEncoder(w).Encode(t); err != nil {
+	// 	panic(err)
+	// }
 }
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
 	// todos = append(todos, repos.Todo{Name: "Getting Started w/ Go"})
@@ -63,6 +55,7 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
 
 }
 func ShowTodo(w http.ResponseWriter, r *http.Request) {
+
 	vars := mux.Vars(r)
 	todoIdStr := vars["todoId"]
 	todoId, _ := strconv.Atoi(todoIdStr)
@@ -72,4 +65,49 @@ func ShowTodo(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	// fmt.Fprintln(w, "Todo show:", todoId)
+}
+func UpdateTodo(w http.ResponseWriter, r *http.Request) {
+	var todo repos.Todo
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		panic(err)
+	}
+	if err := json.Unmarshal(body, &todo); err != nil {
+		respondWithError(w, http.StatusBadRequest, " Bad Payload")
+		return
+	}
+	t, err := repos.UpdateTodo(todo)
+	if err != nil {
+		panic(err)
+		respondWithError(w, http.StatusBadRequest, " Failed to Update")
+		return
+	}
+	respondWithJson(w, http.StatusOK, t)
+}
+func DeleteTodo(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoIdStr := vars["todoId"]
+	todoId, _ := strconv.Atoi(todoIdStr)
+	err := repos.DeleteTodo(todoId)
+
+	// w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	// w.WriteHeader(http.StatusOK)
+	// if err := json.NewEncoder(w).Encode(repos.DeleteTodo(todoId)); err != nil {
+	if err != nil {
+		panic(err)
+		respondWithError(w, http.StatusBadRequest, " Failed to Delete")
+	}
+	respondWithJson(w, http.StatusOK, err)
+	// fmt.Fprintln(w, "Todo show:", todoId)
+}
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJson(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(code)
+	w.Write(response)
 }
--- rest/todo/repos/tododao.go
+++ rest/todo/repos/tododao.go
@@ -2,43 +2,102 @@ package repos
 
 import (
 	"fmt"
+	"log"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var todos Todos
 var currentID int
+var db *mgo.Database
+var todo Todo
 
+type TodoDAO struct {
+	Server   string
+	Database string
+}
+
+const (
+	COLLECTION = "todo"
+)
+
+func (t *TodoDAO) connect() {
+	session, err := mgo.Dial(t.Server)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = session.DB(t.Database)
+}
 func init() {
-	CreateTodo(Todo{Name: " Enhancing repo"})
-	CreateTodo(Todo{Name: " Adding CRUD "})
+	var dao = TodoDAO{}
+	dao.Server = "localhost"
+	dao.Database = "test"
+	dao.connect()
+	// CreateTodo(Todo{Name: " Enhancing repo"})
+	// CreateTodo(Todo{Name: " Adding CRUD "})
 
 }
 
 /* CreateToDo */
 func CreateTodo(t Todo) Todo {
-	currentID += 1
-	t.Id = currentID
+	// currentID++
+	// t.Id = currentID
 
-	todos = append(todos, t)
+	// todos = append(todos, t)
+	err := db.C(COLLECTION).Insert(t)
+	if err != nil {
+		log.Fatal(err)
+	}
 	return t
 
 }
 func FindAllTodo() []Todo {
+	err := db.C(COLLECTION).Find(bson.M{}).All(&todos)
+	if err != nil {
+		log.Fatal(err)
+	}
 	return todos
 }
 func FindTodo(id int) Todo {
-	for _, t := range todos {
-		if t.Id == id {
-			return t
-		}
+	// stringid := strconv.Itoa(id)
+
+	// err := db.C(COLLECTION).FindId(bson.ObjectIdHex(stringid)).One(&todo)
+	err := db.C(COLLECTION).Find(bson.M{"id": id}).One(&todo)
+	if err != nil {
+		log.Fatal(err)
 	}
-	return Todo{}
+	// for _, t := range todos {
+	// 	if t.Id == id {
+	// 		return t
+	// 	}
+	// }
+	return todo
 }
 func DeleteTodo(id int) error {
-	for i, t := range todos {
-		if t.Id == id {
-			todos = append(todos[:i], todos[i+1:]...)
-			return nil
-		}
+
+	err := db.C(COLLECTION).Remove(bson.M{"id": id})
+	// Find(bson.M{"id": id}).One(&todo)
+
+	return err
+	// for i, t := range todos {
+	// 	if t.Id == id {
+	// 		todos = append(todos[:i], todos[i+1:]...)
+	// 		return nil
+	// 	}
+	// }
+	// return fmt.Errorf("Could not find Todo with ID: %d", id)
+}
+
+func UpdateTodo(t Todo) (Todo, error) {
+
+	// todos = append(todos, t)
+	err := db.C(COLLECTION).Find(bson.M{"id": t.Id}).One(&todo)
+
+	if err != nil {
+		return t, fmt.Errorf("Could not find Todo with ID: %d", t.Id)
 	}
-	return fmt.Errorf("Could not find Todo with ID: %d", id)
+	err = db.C(COLLECTION).UpdateId(todo.Id, &t)
+	return t, err
+
 }
